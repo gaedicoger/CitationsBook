b1dc39e037dc61bf687ad17e4024ac4639f4ab01
Gaed <oger.gaedic@gmail.com>
2026-03-06 11:37:17 +0100

    Etape 5 et 6 Ajout citation et creation du compteur

--- script.js
+++ script.js
@@ -2,15 +2,42 @@
 const form = document.getElementById("citation-form");
 //on récupère le dv contenant les citations
 const quoteList = document.getElementById("quote-list");
+let quoteCount = 0;
+
 //on ecoute le submit
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  //on stocke les citations et les auteurs
-  const citation = document.getElementById("citation").value;
-  const author = document.getElementById("author").value;
-  //on cree un nouvel element div
-  const quote = document.createElement("div");
-  //on insere le texte du form dans le nouvel element
-  quote.innerHTML = `<p class="text">${citation}</p><p class="author">${author}</p>`;
-  quoteList.appendChild(quote);
-});
+function listenForm() {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); //Pour éviter le rechargement de la page à chaque submit;
+    //on stocke les citations et les auteurs
+    const citation = document.getElementById("citation").value;
+    const author = document.getElementById("author").value;
+    addQuote(citation, author);
+  });
+  function addQuote(citation, author) {
+    //on crée deux élement p:
+    const textQuote = document.createElement("p");
+    const authorQuote = document.createElement("p");
+    //on insere le texte du form dans les nouveaux éléments p :
+    textQuote.className = "citationText";
+    textQuote.innerText = citation;
+    authorQuote.className = "author";
+    authorQuote.innerText = author;
+    console.log(textQuote);
+    console.log(authorQuote);
+    //On crée un nouvelle div intermédiaire
+    const quote = document.createElement("div");
+    quote.className = "quote"; //on applique la classe à la div
+    //On lie les éléments précédents à la liste:
+    quote.appendChild(textQuote);
+    quote.appendChild(authorQuote);
+    //On relie cette nouvelle div à la liste:
+    quoteList.appendChild(quote);
+    console.log(quoteList);
+    //Compteur:
+    quoteCount += 1; //Je met une incrémentation à chaque citations déposées
+    let count = document.getElementById("count");
+    count.innerText = `${quoteCount} citations`;
+  }
+}
+
+listenForm();
